5f9c49784a13ab6a0c85cc8b7692e80e08b46f0d
nemazeelaura <arvadajohnson@gmail.com>
2017-12-21 08:37:52 -0700

    uploading all files

--- starter-code/app.js
+++ starter-code/app.js
@@ -5,10 +5,14 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
-// How do we 'require' the candyRouter file?
-var candyRouter;
-
 app.use(bodyParser.json());
+// How do we 'require' the candyRouter file?
+var candyRouter = require('./candyRouter');
+
+
+
+
+app.use('/candies', candyRouter);
 
 //How do we redirect the /candies path
 //through our candyRouter?
--- starter-code/candyRouter.js
+++ starter-code/candyRouter.js
@@ -2,16 +2,70 @@ var express = require('express');
 var bodyParser = require('body-parser');
 router = express.Router();
 
-var candies;
+var candies = [{"id":1,"name":"Chewing Gum","color":"Red"},
+              {"id":2,"name":"Pez","color":"Green"}, {"id":3,"name":"Marshmallow","color":"Pink"},
+              {"id":4,"name":"Candy Stick","color":"Blue"}
+
+              ];
 
 //What would need to go into candies
 //in order to pass our first test?
 
 router.get('/', function(req,res) {
-	// What would go here? 
-	// Hint: we want all candies in JSON format
+    // Hint: we want all candies in JSON format
+	res.json(candies);
 });
 
-// Fill out the rest of the routes here
+// Fill out the ret of the routes here
+//get candy by id
+router.get('/:id', function(req, res){
+    var parsedData = parseInt(req.params.id);
+    console.log(typeof(parsedData));
+    for(i =0; i < candies.length; i++){
+        if(parsedData === candies[i].id){
+            res.json(candies[i]);
+        }
+    }
+});
+
+//post to array adding another candy
+router.post('/', function(req,res) {
+    req.body.id = candies.length + 1;
+    candies.push(req.body);
+    res.send(req.body);
+    console.log(req.body);
+});
+
+//updating candy  w id 3 with new info
+router.put('/:id', function(req,res) {
+    var parsedData = parseInt(req.params.id);
+    console.log(parsedData);
+    // candies.parsedData;
+    for(i =0; i < candies.length; i++){
+        if(parsedData === candies[i].id){
+            candies[i] = req.body;
+             res.send(candies[i]);
+        }
+    }
+
+});
+
+// deleting candy with id 2 and adding message deleted
+
+router.delete('/:id', function(req,res) {
+    var parsedData = parseInt(req.params.id);
+    console.log(parsedData);
+      for(i =0; i < candies.length; i++){
+        if(parsedData === candies[i].id){
+            console.log(i);
+            candies.splice(i, 1);
+             res.send({"message":"deleted"});
+        }
+    }
+
+ });
+
+
+
 
 module.exports = router;
